d4e328471890a5762ae6b55c7e351123bbd89090
Basel Al Nassr <mrbasel555@gmail.com>
2020-01-31 16:55:24 +0300

    ''

--- run.py
+++ run.py
@@ -6,8 +6,4 @@ with app.app_context():
     db.create_all()
 
 # if __name__ == '__main__':
-<<<<<<< HEAD
     # app.run(debug=True)
-=======
-    # app.run()
->>>>>>> 9e27f16740eae84415a39a5a7e6b8d604107b148
